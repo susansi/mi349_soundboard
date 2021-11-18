9af37a78405efcc512199952d1f382c1fe791077
susansi <simingji@msu.edu>
2021-11-18 17:28:09 -0500

    push

---
+++ soundboard.js
@@ -0,0 +1,24 @@
+const sounds = ['bloop', 'bubbling', 'neon-light'];
+
+sounds. forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add('btn');
+    
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    });
+    document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.onpause();
+        song.currentTime = 0;
+    });
+}
