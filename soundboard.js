892bd31cff0e37842642f65ef2ea8bdeb29ee223
susansi <simingji@msu.edu>
2021-11-18 19:08:08 -0500

    push

--- soundboard.js
+++ soundboard.js
@@ -13,25 +13,18 @@ let successButton = document.getElementById("success");
 let hmmButton = document.getElementById("hmm");
 let electronicButton = document.getElementById("electronic");
 
-successButton.onmouseenter = function () {
-  playsuccess();
-}
+
 
 successButton.onclick = function () {
   playsuccess();
 }
 
-hmmButton.onmouseenter = function () {
-  playhmm();
-}
+
 
 hmmButton.onclick = function () {
   playhmm();
 }
 
-electronicButton.onmouseenter = function () {
-  playelectronic();
-}
 
 electronicButton.onclick = function () {
   playelectronic();
