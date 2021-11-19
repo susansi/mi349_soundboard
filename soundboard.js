e0829507cd09c6be3537e7333ba5eeda5156ce6a
susansi <simingji@msu.edu>
2021-11-18 19:07:25 -0500

    push

--- soundboard.js
+++ soundboard.js
@@ -1,14 +1,38 @@
-function play1() {
-    var success = document.getElementById("success");
-    audio1.play();
+function playsuccess() {
+    document.getElementById("success").play();
   }
 
-  function play2() {
-    var hmm = document.getElementById("hmm");
-    audio2.play();
+  function playhmm() {
+    document.getElementById("hmm").play();
   }
 
-  function play3() {
-    var electronic = document.getElementById("electronic");
-    audio3.play();
+  function playelectronic() {
+    document.getElementById("electronic").play();
   }
+let successButton = document.getElementById("success");
+let hmmButton = document.getElementById("hmm");
+let electronicButton = document.getElementById("electronic");
+
+successButton.onmouseenter = function () {
+  playsuccess();
+}
+
+successButton.onclick = function () {
+  playsuccess();
+}
+
+hmmButton.onmouseenter = function () {
+  playhmm();
+}
+
+hmmButton.onclick = function () {
+  playhmm();
+}
+
+electronicButton.onmouseenter = function () {
+  playelectronic();
+}
+
+electronicButton.onclick = function () {
+  playelectronic();
+}
